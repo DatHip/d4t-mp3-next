b2efe0518ad472a9f4918c15bdf7478346fe9c22
DatHip <83833987+DatHip@users.noreply.github.com>
2023-06-07 12:57:26 +0700

    -Update Header

--- src/components/layouts/Main/MainHeader.tsx
+++ src/components/layouts/Main/MainHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
-export default function MainHeader () {
-  return <header>MainHeader</header>
+export default function MainHeader() {
+  return <header className='fixed zm13:left-[240px] right-0 z-[99] h-[70px] px-[var(--padding-section)] top-0 min-w-[660px] '>
+    <div></div>
+  </header>
 }
--- src/components/layouts/Main/SliderLeft.tsx
+++ src/components/layouts/Main/SliderLeft.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
 export default function SliderLeft() {
-  return <aside className='fixed z-[100] h-[calc(100vh-90px)] zm13:relative'>SliderLeft</aside>
+  return <aside className='fixed z-[100] h-[calc(100vh-90px)] zm13:relative'>
+    <div>Logo</div>
+    <div>List NavLink</div>
+  </aside>
 }

9a0f3a0335d2065e6f88e4a05ed5051303c58327
DatHip <83833987+DatHip@users.noreply.github.com>
2023-07-06 17:55:02 +0700

    -Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import ChangeTheme from "@/components/common/ChangeTheme";
 import MainLayout from "@/components/layouts/Main/MainLayout";
 import ReduxStore from "@/store";
 import { MantineProvider } from "@mantine/core";
 import { SessionProvider } from "next-auth/react";
-import { useState } from "react";
 import "@/styles/grid.css";
 import "@/styles/root.css";
 import "@/styles/iconZs.css";
 import "@/styles/globals.css";
 import "tippy.js/dist/tippy.css";
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { type Session } from "next-auth";
 import { type AppType } from "next/app";
 import {
@@ -20,23 +19,28 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export const queryClient = new QueryClient();
+
 const MyApp: AppType<{
   session: Session | null | any;
   dehydratedState: any;
 }> = ({ Component, pageProps: { session, ...pageProps } }) => {
-  const [queryClient] = useState(() => new QueryClient());
-
+  const [queryClient2] = useState(queryClient);
   return (
     <ReduxStore>
       <SessionProvider session={session}>
         <MantineProvider>
           <ChangeTheme></ChangeTheme>
-          <QueryClientProvider client={queryClient}>
+          <QueryClientProvider client={queryClient2}>
             <Hydrate state={pageProps?.dehydratedState}>
               <MainLayout>
                 <Component {...pageProps} />
               </MainLayout>
             </Hydrate>
+            <ReactQueryDevtools></ReactQueryDevtools>
           </QueryClientProvider>
         </MantineProvider>
       </SessionProvider>

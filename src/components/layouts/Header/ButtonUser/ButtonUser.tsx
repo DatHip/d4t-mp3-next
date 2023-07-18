62185d92547b90256fde2c24aa3f6c652cc79a9c
DatHip <83833987+DatHip@users.noreply.github.com>
2023-07-18 18:05:37 +0700

    -Update

--- src/components/layouts/Header/ButtonUser/ButtonUser.tsx
+++ src/components/layouts/Header/ButtonUser/ButtonUser.tsx
@@ -1,22 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
-import Tippy from '@tippyjs/react'
+import Image from "next/image";
+import React from "react";
+import Tippy from "@tippyjs/react";
 
 const ButtonUser = () => {
   return (
-    <Tippy content='Cá nhân'>
-      <div className='flex items-center justify-center w-10 h-10 overflow-hidden rounded-full cursor-pointer hover:shadow-md'>
+    <Tippy content="Cá nhân">
+      <div className="flex h-10 w-10 cursor-pointer items-center justify-center overflow-hidden rounded-full hover:shadow-md">
         <figure>
           <Image
             width={40}
             height={40}
-            alt='avatar-d4t'
-            src='https://avatar.talk.zdn.vn/default'
+            alt="avatar-d4t"
+            src="https://avatar.talk.zdn.vn/default"
           ></Image>
         </figure>
       </div>
     </Tippy>
-  )
-}
+  );
+};
 
-export default ButtonUser
+export default ButtonUser;
